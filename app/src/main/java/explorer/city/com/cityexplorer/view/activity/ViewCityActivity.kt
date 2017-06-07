c9fc2b63ddd7a06553ed5845aa8aafb77bac4d23
Martin Tøften <martin@bakkenbaeck.no>
2017-06-07 11:37:26 +0200

    Add ViewCityActvity

---
+++ app/src/main/java/explorer/city/com/cityexplorer/view/activity/ViewCityActivity.kt
@@ -0,0 +1,13 @@
+package explorer.city.com.cityexplorer.view.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import explorer.city.com.cityexplorer.R
+
+class ViewCityActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_city)
+    }
+}
