bd5edb6cfb60945d9b74fa09adf478e4ab85950c
Martin Tøften <martin@bakkenbaeck.no>
2017-06-09 11:43:30 +0200

    Add data to view

--- app/src/main/java/explorer/city/com/cityexplorer/view/activity/DetailsActivity.kt
+++ app/src/main/java/explorer/city/com/cityexplorer/view/activity/DetailsActivity.kt
@@ -1,10 +1,15 @@
 package explorer.city.com.cityexplorer.view.activity
 
+import android.arch.lifecycle.LifecycleActivity
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import explorer.city.com.cityexplorer.R
+import explorer.city.com.cityexplorer.model.CityDetail
+import explorer.city.com.cityexplorer.viewModel.DetailsViewModel
+import kotlinx.android.synthetic.main.activity_details.*
 
-class DetailsActivity : AppCompatActivity() {
+class DetailsActivity : LifecycleActivity() {
 
     companion object {
         val CATEGORY_NAME = "CATEGORY_NAME"
@@ -14,5 +19,34 @@ class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+
+        initClickListeners()
+        initToolbar()
+        initObservers()
+        getDetails()
     }
+
+    private fun initClickListeners() {
+        backArrow.setOnClickListener { finish() }
+    }
+
+    private fun initToolbar() {
+        toolbarTitle.text = getCategoryName()
+    }
+
+    private fun initObservers() {
+        val model = ViewModelProviders.of(this).get(DetailsViewModel::class.java)
+        model.liveDetails.observe(this, Observer { updateDetailsUi(it) })
+    }
+
+    private fun updateDetailsUi(cityDetails: List<CityDetail>?) {}
+
+    private fun getDetails() {
+        val model = ViewModelProviders.of(this).get(DetailsViewModel::class.java)
+        model.fetchDetails(getUrl(), getCategoryName())
+    }
+
+    private fun getUrl() = intent.getStringExtra(URL)
+
+    private fun getCategoryName() = intent.getStringExtra(CATEGORY_NAME)
 }
